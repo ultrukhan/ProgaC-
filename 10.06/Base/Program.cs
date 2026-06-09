b59689c9597194bb84e54efb54a172d9ff5c5928
Uliana Trukhan <ul.trukhan07@gmail.com>
2026-06-10 02:15:05 +0300

    Add base files

---
+++ 10.06/Base/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Pidgot_4
+{
+    public class Logic
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var  = XDocument.Load(".xml").Descendants("");
+            var  = XDocument.Load(".xml").Descendants("");
+            var  = XDocument.Load(".xml").Descendants("");
+            var  = XDocument.Load(".xml").Descendants("");
+            var  = XDocument.Load(".xml").Descendants("");
+            var = .Concat();
+
+            var taskA = ;
+            taskA.Save("TaskA.xml");
+        }
+    }
+}
---
+++ 10.06/Base/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Pidgot_4; // змінити
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Data;
+using System.Collections.Generic;
+
+namespace TestPidgot_4
+{
+    public class Fixture
+    {
+        public IEnumerable<XElement>  { get; private set; }
+        public IEnumerable<XElement>  { get; private set; }
+        public IEnumerable<XElement>  { get; private set; }
+        public IEnumerable<XElement>  { get; private set; }
+        public IEnumerable<XElement>  { get; private set; }
+        public IEnumerable<XElement>  { get; private set; }
+
+        public Fixture()
+        {
+             = XElement.Parse("@").Descendants("");
+             = XElement.Parse("@").Descendants("");
+             = XElement.Parse("@").Descendants("");
+             = XElement.Parse("@").Descendants("");
+             = XElement.Parse("@").Descendants("");
+
+        }
+    }
+    public class UnitTest1 : IClassFixture<Fixture>
+    {
+        public readonly Fixture _fixture;
+        public UnitTest1(Fixture fixture)
+        {
+            _fixture = fixture;
+        }
+        [Fact]
+        public void TaskATest()
+        {
+            var exp = XElement.Parse("@");
+            var res = Logic.TaskA();
+            Assert.True(XNode.DeepEquals(exp, res), "Trees don`t match!");
+        }
+        [Fact]
+        public void TaskBTest()
+        {
+            var exp = XElement.Parse("@");
+            var res = Logic.TaskB();
+            Assert.True(XNode.DeepEquals(exp, res), "Trees don`t match!");
+        }
+    }
+}
